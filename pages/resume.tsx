2aa8fdd36d52a19b4aaeba029402f4234bb179af
gnamikawa <genzo.namikawa@outlook.com>
2021-12-22 21:41:48 -0800

    basic styling and mdx updates

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -3,7 +3,8 @@ import { getHeapStatistics } from 'v8';
 import { Fullscreen } from '../components/fullscreen';
 import Resumed from '@/markdown/resume.mdx';
 import s from '@/styles/mdx.module.scss';
-
+import {run} from '@mdx-js/mdx'
+import * as runtime from 'react/jsx-runtime.js'
 
 export async function getStaticProps(context) {
   return {
@@ -15,9 +16,12 @@ export async function getStaticProps(context) {
 
 export default function Resume(props) {
   return <>
-    <div className="mx-10 my-20">
-      <div className={s.mdx}>
-        <Resumed/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/styles/github-dark.min.css"></link>
+    <div className="bg-gray-100 py-10">
+      <div className='rounded-md bg-white w-fit mx-auto p-10 shadow-sm'>
+        <div className={s.mdx}>
+          <Resumed/>
+        </div>
       </div>
     </div>
   </>
