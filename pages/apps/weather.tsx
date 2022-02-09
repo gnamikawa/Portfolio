48db4e529634c5f8f335be5bf1e51dd365ff63d4
Genzo Namikawa <genzo.namikawa@jacks.sdstate.edu>
2022-02-09 23:55:50 +0900

    simple d3 daily weather

--- pages/apps/weather.tsx
+++ pages/apps/weather.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { useEffect, useState } from 'react';
 import * as d3 from "d3";
-import {isEmpty} from 'lodash';
+import {isEmpty, isNull, maxBy, minBy} from 'lodash';
 // import styles from '../styles/Home.module.scss'
 
 interface Errorable {
@@ -50,14 +50,13 @@ const useUserWeather = () => {
 }
 const useUserForecast = (cnt) => {
     const {success: geolocationSuccess, error: geolocationError} = useGeolocation();
-    const [openWeatherData, setOpenWeatherData] = useState({});
+    const [openWeatherData, setOpenWeatherData] = useState<any>({});
     useEffect(() => {
         if(!geolocationSuccess) return;
         (async () => {
             const api = '4fd5e62091f720b613ba466a48d80656';
             const [lat, lon] = [geolocationSuccess.coords.latitude, geolocationSuccess.coords.longitude];
             const endpoint = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${api}`;
-            console.log(endpoint);
             const openWeatherSuccess = await fetch(endpoint).then(raw => raw.json());
             setOpenWeatherData(openWeatherSuccess);
         })();
@@ -69,20 +68,32 @@ export default function Home() {
     const forecast = useUserForecast(7);
     console.log(forecast);
     useEffect( () => {
-        if(isEmpty(forecast)) return;
+        if(isEmpty(forecast) || isNull(forecast)) return;
+        const [dailyMin, dailyMax] = [minBy(forecast.daily, x => x.temp.day), maxBy(forecast.daily, x => x.temp.day)];
+        console.log(dailyMin, dailyMax)
+        const xScale = d3
+            .scaleLinear()
+            .domain( [dailyMin.temp.day, dailyMax.temp.day] )
+            .range( [300, 0] );
+        const yScale = d3
+            .scaleBand()
+            .domain(forecast.daily.map(x => x))
+            .rangeRound( [0,300] )
+            .padding(0.1);
         const tempRects = d3.select("svg")
-            .selectAll("rect")
-            .data(forecast?.daily)
+            .selectAll(".bar")
+            .data(forecast.daily)
             .enter()
             .append("rect")
-            .attr("x", 10)
-            .attr("y", (d,i) => i * 15 + 10)
-            .attr("width", (d,i) => 100 * (d.temp.max - d.temp.day))
-            .attr("height", 10)
+            .classed('bar', true)
+            .attr("width", (d,i) => 300 - xScale(d.temp.day))
+            .attr("height", yScale.bandwidth())
+            .attr("x", 0)
+            .attr("y", (d,i) => yScale(d))
             .attr("fill", "#333");
 
     }, [forecast] );
     return <>
-        <svg width='300' height='300'></svg>
+        <svg width='300' height='300'/>
     </>
 }
