3fb0a980c023d57bee02ef5febd9ce56dc66a6a3
Genzo Namikawa <genzo.namikawa@jacks.sdstate.edu>
2022-05-11 22:38:01 +0900

    removed useless components

--- components/asdf.tsx
+++
@@ -1,3 +0,0 @@
-export function Asdf(props){
-    return <div className="text-red-500">wow!</div>;
-}
--- components/fullscreen.tsx
+++
@@ -1,7 +0,0 @@
-export function Fullscreen(props){
-    return <>
-        <div className='h-full w-full relative bg-indigo-500'>
-            {props.children}
-        </div>
-    </>
-}
