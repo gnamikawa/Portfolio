086c6a56f99a626617a87d719399e4d2746e9c76
Genzo <genzo.namikawa@outlook.com>
2023-10-22 21:00:00 +0900

    refactor: update style

--- pages/api/weather/forecast/week.tsx
+++ pages/api/weather/forecast/week.tsx
@@ -1,14 +1,12 @@
 export default async function handler(req, res) {
-    
-    const api = process.env.OPENWEATHERMAP_API;
-    const [lat, lon] = [req.query.lat, req.query.lon];
+  const api = process.env.OPENWEATHERMAP_API;
+  const [lat, lon] = [req.query.lat, req.query.lon];
 
-    const endpoint = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${api}`;
-    const openWeatherSuccess = await fetch(endpoint).then(raw => raw.json());
+  const endpoint = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${api}`;
+  const openWeatherSuccess = await fetch(endpoint).then((raw) => raw.json());
 
-    res.setHeader('Content-Type', 'application/json');
-    res.send({
-        openWeather: openWeatherSuccess
-    });
-    
+  res.setHeader("Content-Type", "application/json");
+  res.send({
+    openWeather: openWeatherSuccess,
+  });
 }
