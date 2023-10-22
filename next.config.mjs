d8416c894bd53363392af218bdb6c0243d629915
Genzo <genzo.namikawa@outlook.com>
2023-10-22 21:49:55 +0900

    build: create working configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,45 @@
-import { PHASE_DEVELOPMENT_SERVER } from "next/constants";
+import {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} from "next/constants.js";
+import path from "path";
+import { URL } from "url";
 
-/**
- * @type {import('next').NextConfig}
- */
-const baseNextConfig = {
-  /* config options here */
-  swcMinify: true,
-};
+const __filename = new URL("", import.meta.url).pathname;
+const __dirname = new URL(".", import.meta.url).pathname;
 
-export default baseNextConfig;
+const nextConfig = (phase) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  let config = {};
 
-module.exports = (phase) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      ...baseNextConfig,
-    };
+  switch (phase) {
+    case PHASE_PRODUCTION_BUILD: {
+    }
+
+    case PHASE_DEVELOPMENT_SERVER: {
+      config = {
+        lessLoaderOptions: {
+          javascriptEnabled: true,
+        },
+        experimental: { esmExternals: true },
+        pageExtensions: ["md", "mdx", "tsx", "ts", "jsx", "js"],
+        sassOptions: {
+          includePaths: [path.join(__dirname, "styles")],
+        },
+        webpack: (config, { defaultLoaders }) => {
+          config.module.rules.push({
+            test: /\.(md|mdx)$/,
+            use: [defaultLoaders.babel, "@mdx-js/loader"],
+          });
+          return config;
+        },
+      };
+    }
   }
 
-  return baseNextConfig;
+  return config;
 };
+
+export default nextConfig;
